0d9f2aba2ce7b36d6e0fd43bdcf1acc05af8f70f
Jusky92 <julienboushera92@gmail.com>
2026-03-17 15:51:03 +0100

    Page products + historique (Transaction acheté)

---
+++ src/app/core/services/transaction.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export interface Transaction {
+  nom: string;
+  date?: string;
+  type_mouvement: string;
+  quantite: number;
+  categorie: string;
+  prix_avant_promo: number;
+  remise: number;
+  prix_unitaire: number;
+  total: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionService {
+
+  private readonly transactionsUrl = environment.API_URL + 'distant/transactions/';
+
+  constructor(private http: HttpClient) {}
+
+  getTransactions(): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(this.transactionsUrl);
+  }
+
+  addTransaction(transaction: Transaction): Observable<Transaction> {
+    return this.http.post<Transaction>(this.transactionsUrl, transaction);
+  }
+}
--- src/app/pages/historique/historique.component.ts
+++ src/app/pages/historique/historique.component.ts
@@ -1,18 +1,33 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SidebarStateService } from 'src/app/core/services/sidebar-state.service';
 import { SidebarComponent } from 'src/app/shared/sidebar/sidebar.component';
+import { TransactionService, Transaction } from 'src/app/core/services/transaction.service';
 
 @Component({
   selector: 'app-historique',
-  imports: [SidebarComponent],
+  imports: [CommonModule, SidebarComponent],
   templateUrl: './historique.component.html',
   styleUrl: './historique.component.css'
 })
-export class HistoriqueComponent {
-  constructor(private readonly sidebarStateService: SidebarStateService) {}
+export class HistoriqueComponent implements OnInit {
+
+  transactions: Transaction[] = [];
+
+  constructor(
+    private readonly sidebarStateService: SidebarStateService,
+    private readonly transactionService: TransactionService
+  ) {}
 
   @HostBinding('style.margin-left.px')
   get hostMarginLeft(): number {
     return this.sidebarStateService.isCollapsed ? 50 : 180;
+  }
+
+  ngOnInit() {
+    this.transactionService.getTransactions().subscribe({
+      next: (data) => this.transactions = data,
+      error: (err) => console.error('Erreur chargement historique', err)
+    });
   }
 }
